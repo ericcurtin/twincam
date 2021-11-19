b6a9753b394a94fa58c341faa468abe5d79b499b
Eric Curtin <ecurtin@redhat.com>
2021-11-19 16:40:09 +0000

    Add commit_sync code

--- twincam.cpp
+++ twincam.cpp
@@ -33,17 +33,18 @@ using libcamera::StreamRole;
 using kms::AtomicReq;
 using kms::Card;
 using kms::Connector;
+using kms::Crtc;
 using kms::DumbFramebuffer;
 using kms::Framebuffer;
 using kms::Plane;
+using kms::PlaneType;
 using kms::Videomode;
 
 static const char* usage_str =
     "Usage: twincam [OPTIONS]\n\n"
     "Options:\n"
-    "  -s, --single                Single camera mode. Open only /dev/video0\n"
-    "  -a, --api=<drm|v4l>         Use DRM or V4L API. Default: DRM\n"
     "  -l, --list-displays         List displays\n"
+    "  -c, --list-cameras          List cameras\n"
     "  -h, --help                  Print this help\n";
 
 static std::string PixelFormatToCC(const kms::PixelFormat& f) {
@@ -97,7 +98,12 @@ int main(int argc, char** argv) {
         "\tConnectors:\n",
         card.is_master(), card.has_atomic(), card.has_universal_planes(),
         card.has_dumb_buffers(), card.has_kms(), card.version_name().c_str());
-    for (Connector* c : card.get_connectors()) {
+  }
+
+  Plane* plane;
+  Crtc* ctrc = card.get_connectors().front()->get_current_crtc();
+  for (Connector* c : card.get_connectors()) {
+    if (list_displays) {
       print(
           "\tfullname: {:s}\n"
           "\tconnected: {:d}\n"
@@ -106,12 +112,50 @@ int main(int argc, char** argv) {
           "\tmmWidth: {:d}\n"
           "\tmmHeight: {:d}\n"
           "\tsubpixel: {:d}\n"
-          "\tsubpixel_str: {:s}\n\n"
-          "{:s}",
+          "\tsubpixel_str: {:s}\n\n",
           c->fullname().c_str(), c->connected(), c->connector_type(),
           c->connector_type_id(), c->mmWidth(), c->mmHeight(), c->subpixel(),
-          c->subpixel_str().c_str(),
-          c->get_modes().empty() ? "" : "\t\tVideomodes:\n");
+          c->subpixel_str().c_str());
+    }
+
+    Crtc* this_ctrc = c->get_current_crtc();
+    if (this_ctrc) {
+      if (list_displays) {
+        print(
+            "\t\tget_current_crtc:\n"
+            "\t\tbuffer_id: {:d}\n"
+            "\t\tx: {:d}\n"
+            "\t\ty: {:d}\n"
+            "\t\twidth {:d}\n"
+            "\t\theight: {:d}\n"
+            "\t\tmode_valid: {:d}\n"
+            "\t\tlegacy_gamma_size: {:d}\n",
+            this_ctrc->buffer_id(), this_ctrc->x(), this_ctrc->y(),
+            this_ctrc->width(), this_ctrc->height(), this_ctrc->mode_valid(),
+            this_ctrc->legacy_gamma_size());
+      }
+
+      for (Plane* p : ctrc->get_possible_planes()) {
+        if (p->crtc_id()) {
+          if (list_displays) {
+            print("\t\tcrtc_id: {:d}\n\n", p->crtc_id());
+          }
+
+          plane = p;
+          break;
+        }
+      }
+    }
+
+    if (list_displays) {
+      if (!c->get_modes().empty()) {
+        print("\t\tVideomodes:\n");
+      }
+
+      //       : "", c->get_current_crtc()->buffer_id(), c->get_current_crtc() ?
+      //       '\n' : '',
+      //    join((std::vector<void*>)c->get_possible_crtcs(), ", "),
+      //  c->get_modes().empty() ? "" : "\t\tVideomodes:\n");
       for (Videomode& v : c->get_modes()) {
         print(
             "\t\tto_string_long: {:s}\n"
@@ -127,45 +171,51 @@ int main(int argc, char** argv) {
             v.to_string_long().c_str(), v.hsync_start, v.hsync_end, v.htotal,
             v.hskew, v.vsync_start, v.vsync_end, v.vtotal, v.vscan, v.valid());
       }
-    }
 
-    print("\t\tPlanes:\n");
-    for (Plane* p : card.get_planes()) {
-      print(
-          "\t\tcrtc_id: {:d}\n"
-          "\t\tfb_id: {:d}\n"
-          "\t\tcrtc_x: {:d}\n"
-          "\t\tcrtc_y: {:d}\n"
-          "\t\tx: {:d}\n"
-          "\t\ty: {:d}\n"
-          "\t\tgamma_size: {:d}\n"
-          "\t\tPixelFormatToCC:",
-          p->crtc_id(), p->fb_id(), p->crtc_x(), p->crtc_y(), p->x(), p->y(),
-          p->gamma_size());
-      for (size_t i = 0; i < p->get_formats().size(); ++i) {
-        print("{:s}{:s}", i ? ", " : " ",
-              PixelFormatToCC(p->get_formats()[i]).c_str());
+      print("\tPlanes:\n");
+      for (Plane* p : card.get_planes()) {
+        print(
+            "\tcrtc_id: {:d}\n"
+            "\tfb_id: {:d}\n"
+            "\tcrtc_x: {:d}\n"
+            "\tcrtc_y: {:d}\n"
+            "\tx: {:d}\n"
+            "\ty: {:d}\n"
+            "\tgamma_size: {:d}\n"
+            "\tplane_type: {:d}\n"
+            "\tPixelFormatToCC:",
+            p->crtc_id(), p->fb_id(), p->crtc_x(), p->crtc_y(), p->x(), p->y(),
+            p->gamma_size(), p->plane_type());
+        for (size_t i = 0; i < p->get_formats().size(); ++i) {
+          print("{:s}{:s}", i ? ", " : " ",
+                PixelFormatToCC(p->get_formats()[i]).c_str());
+        }
+
+        print("\n\n");
       }
-
-      print("\n\n");
     }
   }
 
-  if (list_cameras) {
-    setenv("LIBCAMERA_LOG_LEVELS", "2", 1);
-    CameraManager cm;
-    int ret = cm.start();
-    if (ret) {
-      eprint("{:d} = start()\n", ret);
+  Camera* camera = 0;
+  setenv("LIBCAMERA_LOG_LEVELS", "2", 1);
+  CameraManager cm;
+  int ret = cm.start();
+  if (ret) {
+    eprint("{:d} = start()\n", ret);
+  }
+
+  if (!cm.cameras().empty() && list_cameras) {
+    print("Cameras:\n");
+  }
+
+  const std::vector<std::shared_ptr<Camera>>& cameras = cm.cameras();
+  for (size_t cam_num = 0; cam_num < cameras.size(); ++cam_num) {
+    const std::shared_ptr<Camera>& cam = cameras[cam_num];
+    if (!camera) {
+      camera = cam.get();
     }
 
-    if (!cm.cameras().empty()) {
-      print("Cameras:\n\n");
-    }
-
-    const std::vector<std::shared_ptr<Camera>>& cameras = cm.cameras();
-    for (size_t cam_num = 0; cam_num < cameras.size(); ++cam_num) {
-      const std::shared_ptr<Camera>& cam = cameras[cam_num];
+    if (list_cameras) {
       const ControlList& props = cam->properties();
       print(
           "cam_num: {:d}\n"
@@ -250,6 +300,38 @@ int main(int argc, char** argv) {
     }
   }
 
+  if (!ctrc) {
+    eprint("no ctrc set\n");
+    return 0;
+  }
+
+  AtomicReq req(card);
+  req.add(plane, "CRTC_ID", plane->crtc_id());
+  req.add(plane, "FB_ID", plane->fb_id());
+
+  uint32_t x = 0;
+  uint32_t y = 0;
+  uint32_t iw = ctrc->width();
+  uint32_t ih = ctrc->height();
+  uint32_t cam_width = camera->properties().get(PixelArraySize).width;
+  uint32_t cam_height = camera->properties().get(PixelArraySize).height;
+  uint32_t out_x = x + iw / 2 - cam_width / 2;
+  uint32_t out_y = y + ih / 2 - cam_height / 2;
+
+  req.add(plane, "CRTC_X", out_x);
+  req.add(plane, "CRTC_Y", out_y);
+  req.add(plane, "CRTC_W", cam_width);
+  req.add(plane, "CRTC_H", cam_height);
+
+  req.add(plane, "SRC_X", 0);
+  req.add(plane, "SRC_Y", 0);
+  req.add(plane, "SRC_W", cam_width << 16);
+  req.add(plane, "SRC_H", cam_height << 16);
+
+  ret = req.commit_sync();
+  if (ret) {
+    eprint("{:d} = req.commit_sync();\n", ret);
+  }
 #if 0
   for (Connector* c : card.get_connectors()) {
     print(
@@ -289,18 +371,18 @@ int main(int argc, char** argv) {
 
   Framebuffer* fb = m_fb[0];
 
-  req.add(m_plane, "CRTC_ID", m_crtc->id());
-  req.add(m_plane, "FB_ID", fb->id());
+  req.add(plane, "CRTC_ID", m_crtc->id());
+  req.add(plane, "FB_ID", fb->id());
 
-  req.add(m_plane, "CRTC_X", m_out_x);
-  req.add(m_plane, "CRTC_Y", m_out_y);
-  req.add(m_plane, "CRTC_W", m_out_width);
-  req.add(m_plane, "CRTC_H", m_out_height);
+  req.add(plane, "CRTC_X", m_out_x);
+  req.add(plane, "CRTC_Y", m_out_y);
+  req.add(plane, "CRTC_W", m_out_width);
+  req.add(plane, "CRTC_H", m_out_height);
 
-  req.add(m_plane, "SRC_X", 0);
-  req.add(m_plane, "SRC_Y", 0);
-  req.add(m_plane, "SRC_W", m_in_width << 16);
-  req.add(m_plane, "SRC_H", m_in_height << 16);
+  req.add(plane, "SRC_X", 0);
+  req.add(plane, "SRC_Y", 0);
+  req.add(plane, "SRC_W", m_in_width << 16);
+  req.add(plane, "SRC_H", m_in_height << 16);
 
   for (int i = 0; i < 2000; ++i) {
     r = req.commit_sync();
