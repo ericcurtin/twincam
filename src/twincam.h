5e71cc0bc3d91ae05c82d834764f2fba5b51e31f
Eric Curtin <ecurtin@redhat.com>
2021-12-16 18:03:38 +0000

    Remove used enum and have one license location

--- src/twincam.h
+++ src/twincam.h
@@ -1,28 +1,7 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-/*
- * Copyright (C) 2019, Google Inc.
- *
- * twincam.h - Cam application
- */
-
 #pragma once
-
-enum {
-  OptCamera = 'c',
-  OptCapture = 'C',
-  OptDisplay = 'D',
-  OptHelp = 'h',
-  OptInfo = 'I',
-  OptList = 'l',
-  OptListProperties = 'p',
-  OptMonitor = 'm',
-  OptStream = 's',
-  OptListControls = 256,
-  OptStrictFormats = 257,
-  OptMetadata = 258,
-};
 
 #define eprintf(...)              \
   do                              \
     fprintf(stderr, __VA_ARGS__); \
   while (0)
+
