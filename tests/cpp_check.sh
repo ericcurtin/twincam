cad17fe06991c0b8f1c81a688e6a320a84f977aa
Yariv Rachmani <yrachman@redhat.com>
2022-01-31 18:28:34 +0200

    [Fix] Moving apt from workflow to script (#2)
    * [Fix] Moving apt from workflow to script
    
    * adding missing sudo to script
    
    * [suprress errors]
    
    Added
     --suppress=useInitializationList
     --suppress=uninitMemberVar
    
    to cppcheck
    
    * [script fix]

--- tests/cpp_check.sh
+++ tests/cpp_check.sh
@@ -2,10 +2,19 @@
 
 set -e
 
-u="$u -U __REDIRECT_NTH -U _BSD_RUNE_T_ -U _TYPE_size_t -U __LDBL_REDIR1_DECL"
-supp="--suppress=missingInclude --suppress=unusedFunction"
-suppf="redblack.c"
-arg="-q --force $u --enable=all $inc $supp --error-exitcode=1"
-cppcheck="xargs cppcheck $arg"
-
-find . -name "*.[c|h]" | grep -v "$suppf" | $cppcheck  --language=c++ 2>&1
+if [ -n "$CI" ] ; then
+  sudo apt install -y gcc
+  sudo apt install -y make
+  sudo apt install -y autoconf
+  sudo apt install -y automake
+  sudo apt install -y cppcheck
+fi
+ 
+if command -v cppcheck > /dev/null; then
+  u="$u -U __REDIRECT_NTH -U _BSD_RUNE_T_ -U _TYPE_size_t -U __LDBL_REDIR1_DECL"
+  supp="--suppress=missingInclude --suppress=unusedFunction --suppress=noExplicitConstructor"
+  supp="$supp --suppress=useInitializationList --suppress=uninitMemberVar"
+  arg="-q --force $u --enable=all $inc $supp --error-exitcode=1"
+  cppcheck="xargs cppcheck $arg"
+  find . -name  "*.h" -o -name "*.cpp"| $cppcheck  --language=c++ 2>&1
+fi
