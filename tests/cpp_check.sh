03f303476868be9d55bace219814cf07a59b3d09
Yariv Rachmani <yrachman@redhat.com>
2022-01-31 16:08:02 +0200

    [Fix] Moving apt from workflow to script

--- tests/cpp_check.sh
+++ tests/cpp_check.sh
@@ -2,10 +2,19 @@
 
 set -e
 
-u="$u -U __REDIRECT_NTH -U _BSD_RUNE_T_ -U _TYPE_size_t -U __LDBL_REDIR1_DECL"
-supp="--suppress=missingInclude --suppress=unusedFunction"
-suppf="redblack.c"
-arg="-q --force $u --enable=all $inc $supp --error-exitcode=1"
-cppcheck="xargs cppcheck $arg"
-
-find . -name "*.[c|h]" | grep -v "$suppf" | $cppcheck  --language=c++ 2>&1
+if [ -n "$CI" ] ; then
+  apt install -y gcc
+  apt install -y make
+  apt install -y autoconf
+  apt install -y automake
+  apt install -y cppcheck
+fi
+ 
+if command -v cppcheck > /dev/null; then
+  u="$u -U __REDIRECT_NTH -U _BSD_RUNE_T_ -U _TYPE_size_t -U __LDBL_REDIR1_DECL"
+  supp="--suppress=missingInclude --suppress=unusedFunction"
+  suppf="redblack.c"
+  arg="-q --force $u --enable=all $inc $supp --error-exitcode=1"
+  cppcheck="xargs cppcheck $arg"
+  find . -name  "*.h" -o -name "*.cpp"| grep -v "$suppf" | $cppcheck  --language=c++ 2>&1
+fi
