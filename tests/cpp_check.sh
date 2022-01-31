cd1943e9900914e9b7756968cbf6705119ce1500
Yariv Rachmani <yrachman@redhat.com>
2022-01-31 17:42:04 +0200

    [suprress errors]
    Added
     --suppress=useInitializationList
     --suppress=uninitMemberVar
    
    to cppcheck

--- tests/cpp_check.sh
+++ tests/cpp_check.sh
@@ -12,8 +12,8 @@ fi
  
 if command -v cppcheck > /dev/null; then
   u="$u -U __REDIRECT_NTH -U _BSD_RUNE_T_ -U _TYPE_size_t -U __LDBL_REDIR1_DECL"
-  supp="--suppress=missingInclude --suppress=unusedFunction"
-  suppf="redblack.c"
+  supp="--suppress=missingInclude --suppress=unusedFunction --suppress=noExplicitConstructor"
+  supp="$supp --suppress=useInitializationList --suppress=uninitMemberVar"
   arg="-q --force $u --enable=all $inc $supp --error-exitcode=1"
   cppcheck="xargs cppcheck $arg"
   find . -name  "*.h" -o -name "*.cpp"| grep -v "$suppf" | $cppcheck  --language=c++ 2>&1
