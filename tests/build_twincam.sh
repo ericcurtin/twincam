f42ce47e17065ceea4839170d57fc4f3329c5176
Yariv Rachmani <yrachman@redhat.com>
2022-02-03 13:22:17 +0200

    [github-ci] Add twincam build (#5)
    * [github-ci] Add twincam build
    
    Build libcam,
    Build twincam,
    
    * [change] Modify single gate workflow
    
    Moving to single workflow
    .github/workflows/testgate.yml
    workflow job contains 3 steps
    tests scripts updated to the follwoing:
    * prepare_env.sh
    * build_twincam.sh
    * cpp_check.sh

--- tests/build_twincam.sh
+++ tests/build_twincam.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+echo "check if libcamera exist" 
+if [ -d "../libcamera" ]; then
+  rm -rf ../libcamera
+fi
 
+cd ..
+echo "$(pwd)"
+echo "clone libcamera" 
 git clone https://github.com/kbingham/libcamera.git
 cd libcamera
+echo "build libcamera" 
 meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install
-cd -
-git clone https://github.com/ericcurtin/twincam
-cd twincam
+cd ../twincam
+echo "$(pwd)"
+
+echo "build twincam" 
 meson build --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install
