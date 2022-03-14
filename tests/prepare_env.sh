8051a6031f60f2db02065e4bdffeafd25f7fbb38
Eric Curtin <ericcurtin17@gmail.com>
2022-03-14 11:08:43 +0000

    Azure mirror failing in github build
    Hopefully apt update fixes it

--- tests/prepare_env.sh
+++ tests/prepare_env.sh
@@ -3,24 +3,10 @@
 set -e
 
 if [ -n "$CI" ] ; then
-# required for build 
-  sudo apt install -y clang
-  sudo apt install -y gnutls-dev
-  sudo apt install -y libboost-dev
-  sudo apt install -y meson
-  sudo apt install -y python3-pip
-  python3 -m pip install jinja2
-  python3 -m pip install ply
-  sudo apt install -y cmake
-  sudo apt install -y pkg-config
-  sudo apt install -y libevent-dev
-  sudo apt install -y libdrm-dev
-# required for cppcheck
-  sudo apt install -y gcc
-  sudo apt install -y make
-  sudo apt install -y autoconf
-  sudo apt install -y automake
-  sudo apt install -y cppcheck
+  sudo apt update
+  sudo apt install -y clang gnutls-dev libboost-dev meson python3-jinja2 \
+    python3-ply python3-yaml cmake pkg-config libevent-dev libdrm-dev gcc make \
+    autoconf automake cppcheck
 else
   echo "skipping building packages"
 fi
