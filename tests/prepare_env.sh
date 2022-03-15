faa311b2fd110c283ec7b8a802b0a62448f371b5
Eric Curtin <ecurtin@redhat.com>
2022-03-15 10:42:07 +0000

    Add environment variable so we never prompt
    CI is not interactive

--- tests/prepare_env.sh
+++ tests/prepare_env.sh
@@ -3,10 +3,22 @@
 set -e
 
 if [ -n "$CI" ] ; then
-  sudo apt update
-  sudo apt install -y clang gnutls-dev libboost-dev meson python3-jinja2 \
-    python3-ply python3-yaml cmake pkg-config libevent-dev libdrm-dev gcc make \
-    autoconf automake cppcheck
+  export DEBIAN_FRONTEND=noninteractive
+  sudo apt update || true
+  sudo apt install -y clang || true
+  sudo apt install -y gnutls-dev || true
+  sudo apt install -y libboost-dev || true
+  sudo apt install -y meson || true
+  sudo apt install -y python3-jinja2 || true
+  sudo apt install -y python3-ply || true
+  sudo apt install -y python3-yaml || true
+  sudo apt install -y cmake || true
+  sudo apt install -y pkg-config || true
+  sudo apt install -y libevent-dev || true
+  sudo apt install -y libdrm-dev gcc make || true
+  sudo apt install -y autoconf || true
+  sudo apt install -y automake || true
+  sudo apt install -y cppcheck || true
 else
   echo "skipping building packages"
 fi
