0135a94ba0ad89b7b3902bf50ab23f4358ef038b
Eric Curtin <ecurtin@redhat.com>
2022-04-20 17:23:11 +0100

    dracut module-setup.sh file for twincam
    Extra files needed for SDL2.0 version

--- lib/dracut/modules.d/81twincam/module-setup.sh
+++ lib/dracut/modules.d/81twincam/module-setup.sh
@@ -7,7 +7,7 @@ check() {
 
 # called by dracut
 depends() {
-   echo drm
+    echo drm
 }
 
 # Install kernel module(s).
@@ -17,10 +17,22 @@ installkernel() {
 
 # called by dracut
 install() {
-        inst /usr/bin/twincam
-        # install libs
-        inst_libdir_file "libcamera*.so*"
-        inst_libdir_file "libevent*.so*"
-        inst_libdir_file "libstdc++.so*"
+    inst /usr/bin/twincam
+    # install libs
+    inst_libdir_file "libcamera*.so*"
+    inst_libdir_file "libcamera/ipa_*.so*"
+    inst_libdir_file "libevent*.so*"
+    inst_libdir_file "libstdc++.so*"
+
+    # Required if using Fedora SDL2
+    inst_libdir_file "libSDL2*.so*"
+    inst_libdir_file "libgbm*.so*"
+    inst_libdir_file "dri/*.so*"
+    inst_libdir_file "libOpenGL*.so*"
+    inst_libdir_file "libEGL*.so*"
+    inst_libdir_file "egl_vendor.d/*.json"
+
+    inst_dir "/usr/share/glvnd/egl_vendor.d"
+    inst_multiple "/usr/share/glvnd/egl_vendor.d/*.json"
 }
 
