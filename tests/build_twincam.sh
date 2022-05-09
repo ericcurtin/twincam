ba41d6ef2717daffea7cbceae2eec3b8b7e26c02
Eric Curtin <ecurtin@redhat.com>
2022-05-09 20:23:30 +0100

    build with clang also

--- tests/build_twincam.sh
+++ tests/build_twincam.sh
@@ -7,16 +7,28 @@ if [ -d "../libcamera" ]; then
   rm -rf ../libcamera
 fi
 
+build() {
+  meson build --buildtype=release --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install
+}
+
 cd ..
 echo "$(pwd)"
 echo "clone libcamera" 
 git clone https://git.libcamera.org/libcamera/libcamera.git
 cd libcamera
-echo "build libcamera" 
-meson build --buildtype=release --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install
+echo "build libcamera"
+build
 cd ../twincam
 echo "$(pwd)"
 
 echo "build twincam"
 export PKG_CONFIG_PATH="/usr/lib64/pkgconfig/"
-meson build --buildtype=release --prefix=/usr && ninja -v -C build && sudo ninja -v -C build install
+export CC=clang
+export CXX=clang++
+build
+
+git clean -fdx
+export CC=gcc
+export CXX=g++
+build
+
