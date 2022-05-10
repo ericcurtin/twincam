6c077ce022ed664fd55b6a1031a6d74378a52c8a
Eric Curtin <ecurtin@redhat.com>
2022-05-10 10:40:46 +0100

    Fix build
    libyaml added as dependancy

--- tests/prepare_env.sh
+++ tests/prepare_env.sh
@@ -2,33 +2,35 @@
   
 set -e
 
-if [ -n "$CI" ] ; then
-  export DEBIAN_FRONTEND=noninteractive
-  git fetch --unshallow &
-  sudo apt update || true
-  sudo apt install -y clang || true
-  sudo apt install -y gnutls-dev || true
-  sudo apt install -y libboost-dev || true
-  sudo apt install -y meson || true
-  sudo apt install -y python3-jinja2 || true
-  sudo apt install -y python3-ply || true
-  sudo apt install -y python3-yaml || true
-  sudo apt install -y cmake || true
-  sudo apt install -y pkg-config || true
-  sudo apt install -y libevent-dev || true
-  sudo apt install -y libdrm-dev || true
-  sudo apt install -y gcc || true
-  sudo apt install -y make || true
-  sudo apt install -y autoconf || true
-  sudo apt install -y automake || true
-  sudo apt install -y cppcheck || true
-  sudo apt install -y libsdl2-dev || true
-  sudo apt install -y meson || true
-  sudo apt install -y git || true
-  sudo apt install -y vim || true
-  sudo apt install -y python3-jinja2 || true
-  sudo apt install -y python3-ply || true
-  sudo apt install -y libgtest-dev || true
-else
-  echo "skipping building packages"
+if [ "$EUID" -ne 0 ]; then
+  prefix="sudo"
 fi
+
+export DEBIAN_FRONTEND=noninteractive
+$prefix apt update || true
+$prefix apt install -y git || true
+git fetch --unshallow &
+$prefix apt install -y clang || true
+$prefix apt install -y gnutls-dev || true
+$prefix apt install -y libboost-dev || true
+$prefix apt install -y meson || true
+$prefix apt install -y python3-jinja2 || true
+$prefix apt install -y python3-ply || true
+$prefix apt install -y python3-yaml || true
+$prefix apt install -y cmake || true
+$prefix apt install -y pkg-config || true
+$prefix apt install -y libevent-dev || true
+$prefix apt install -y libdrm-dev || true
+$prefix apt install -y gcc || true
+$prefix apt install -y make || true
+$prefix apt install -y autoconf || true
+$prefix apt install -y automake || true
+$prefix apt install -y cppcheck || true
+$prefix apt install -y libsdl2-dev || true
+$prefix apt install -y meson || true
+$prefix apt install -y vim || true
+$prefix apt install -y python3-jinja2 || true
+$prefix apt install -y python3-ply || true
+$prefix apt install -y libgtest-dev || true
+$prefix apt install -y libyaml-dev || true
+
