710c24ebbc9a600ef1a64905b40264ba828eee88
Eric Curtin <ecurtin@redhat.com>
2022-05-18 19:59:33 +0100

    zero-copy strings

--- src/string_printf.cpp
+++ src/string_printf.cpp
@@ -4,16 +4,42 @@
 #include <string.h>
 #include <memory>    // For std::unique_ptr
 
+int vasprintf(std::string& str, const char* fmt, va_list args) {
+  int size = 0;
+  va_list tmpa;
+
+  // copy
+  va_copy(tmpa, args);
+
+  // apply variadic arguments to
+  // sprintf with format to get size
+  size = vsnprintf(NULL, 0, fmt, tmpa);
+
+  // toss args
+  va_end(tmpa);
+
+  // return -1 to be compliant if
+  // size is less than 0
+  if (size < 0) {
+    return -1;
+  }
+
+  // alloc with size plus 1 for `\0'
+  str.resize(size);
+
+  // format string with original
+  // variadic arguments and set new size
+  size = vsprintf(&str[0], fmt, args);
+  return size;
+}
+
 std::string string_printf(const char *const fmt, ...) {
   va_list args;
 
   va_start(args, fmt);
-  char* buf;
-  const int size = vasprintf(&buf, fmt, args);
+  std::string ret;
+  vasprintf(ret, fmt, args);
   va_end(args);
-
-  const std::string ret(buf, size);
-  free(buf);
 
   return ret;
 }
