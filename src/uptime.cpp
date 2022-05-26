36944a94da71f492412398ca7dc83e0081107d68
Eric Curtin <ecurtin@redhat.com>
2022-05-26 14:24:25 +0100

    Simplify uptime implementation
    CLOCK_MONOTONIC uses uptime as epoch

--- src/uptime.cpp
+++ src/uptime.cpp
@@ -1,44 +1,10 @@
-#include <fcntl.h>
-#include <locale.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <memory>
-#include <string>
-
-#include "twincam.h"
 #include "uptime.h"
 
 float init_time = 0;
 int uptime(float* up, float* elapsed) {
-  int fd;
-  const char* filename = "/proc/uptime";
-  char buf[32];
-  ssize_t n;
-  if ((fd = open(filename, O_RDONLY)) == -1) {
-    fputs("Error: /proc must be mounted\n", stderr);
-    fflush(NULL);
-    return 102;
-  }
-
-  lseek(fd, 0L, SEEK_SET);
-  if ((n = read(fd, buf, sizeof buf - 1)) < 0) {
-    perror(filename);
-    fflush(NULL);
-    return 103;
-  }
-
-  buf[n] = '\0';
-
-  const std::string savelocale(setlocale(LC_NUMERIC, NULL));
-  setlocale(LC_NUMERIC, "C");
-  if (sscanf(buf, "%f", up) < 1) {
-    setlocale(LC_NUMERIC, savelocale.c_str());
-    fputs("bad data in /proc/uptime\n", stderr);
-    return 104;
-  }
-
+  struct timespec timespec = {0, 0};
+  clock_gettime(CLOCK_MONOTONIC, &timespec);
+  *up = ((timespec.tv_sec * 1000000000) + timespec.tv_nsec) / 1000000000.0;
   if (init_time) {
     *elapsed = *up - init_time;
   } else {
@@ -46,7 +12,6 @@ int uptime(float* up, float* elapsed) {
     *elapsed = 0;
   }
 
-  setlocale(LC_NUMERIC, savelocale.c_str());
   return 0;
 }
 
