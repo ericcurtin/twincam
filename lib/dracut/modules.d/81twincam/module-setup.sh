4b33223e33295b83fea83c427d508192f9e1cd5d
Eric Curtin <ecurtin@redhat.com>
2022-05-31 11:32:08 +0100

    changes

--- lib/dracut/modules.d/81twincam/module-setup.sh
+++ lib/dracut/modules.d/81twincam/module-setup.sh
@@ -28,17 +28,17 @@ install() {
     inst_libdir_file "libstdc++.so*"
 
     # Required if using Fedora SDL2
-    inst_libdir_file "libSDL2*.so*"
-    inst_libdir_file "libgbm*.so*"
-    inst_libdir_file "dri/*.so*"
-    inst_libdir_file "libOpenGL*.so*"
-    inst_libdir_file "libEGL*.so*"
-    inst_libdir_file "egl_vendor.d/*.json"
+#    inst_libdir_file "libSDL2*.so*"
+#    inst_libdir_file "libgbm*.so*"
+#    inst_libdir_file "dri/*.so*"
+#    inst_libdir_file "libOpenGL*.so*"
+#    inst_libdir_file "libEGL*.so*"
+#    inst_libdir_file "egl_vendor.d/*.json"
 
-    inst_dir "/usr/share/glvnd/egl_vendor.d"
-    inst_multiple "/usr/share/glvnd/egl_vendor.d/*.json"
+#    inst_dir "/usr/share/glvnd/egl_vendor.d"
+#    inst_multiple "/usr/share/glvnd/egl_vendor.d/*.json"
 
-    inst_dir "/usr/share/plymouth/themes/external-command/"
-    inst_multiple "/usr/share/plymouth/themes/external-command/*.plymouth"
+#    inst_dir "/usr/share/plymouth/themes/external-command/"
+#    inst_multiple "/usr/share/plymouth/themes/external-command/*.plymouth"
 }
 
